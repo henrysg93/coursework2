d62b4bdee64437c5f42155d9956fa891953b02be
HenrySG <henrysg93@gmail.com>
2014-10-26 11:18:40 +0000

    reverting to index for search

--- search.php
+++ search.php
@@ -16,13 +16,14 @@
 </style>
 </head>
 <body>
-<h1>Henry Scott-Green Fan Club - Search Page!</h1>
-<p>Fill in your name and email address, then click <strong>Search</strong> to search.</p>
-<p> <a href="http://henrysg93.azurewebsites.net/index.php">Back to signups</a> </p>
+<h1>Henry Scott-Green Fan Club - Sign Up Form!</h1>
+<p>Fill in your name and email address, then click <strong>Submit</strong> to register.</p>
+<p> <a href="http://henrysg93.azurewebsites.net/search.php">Search records now!</a> </p>
 <form method="post" action="index.php" enctype="multipart/form-data" >
-      
+      Name  <input type="text" name="name" id="name"/></br>
+      Email <input type="text" name="email" id="email"/></br>
 	  Company <input type="text" name="company" id="company"/></br>
-      <input type="search" name="search" value="search" />
+      <input type="submit" name="submit" value="Submit" />
 </form>
 <?php
     // DB connection info
@@ -43,17 +44,27 @@
     // Insert registration info
     if(!empty($_POST)) {
     try {
-       
+        $name = $_POST['name'];
+        $email = $_POST['email'];
 		$company = $_POST['company'];
-       
+        $date = date("Y-m-d");
+        // Insert data
+        $sql_insert = "INSERT INTO registration_tbl (name, email, date, company) 
+                   VALUES (?,?,?,?)";
+        $stmt = $conn->prepare($sql_insert);
+        $stmt->bindValue(1, $name);
+        $stmt->bindValue(2, $email);
+        $stmt->bindValue(3, $date);
+		$stmt->bindValue(4, $company);
+        $stmt->execute();
     }
     catch(Exception $e) {
         die(var_dump($e));
     }
-    echo "<h3>You're registered!</h3>"; 
+    echo "<h3>You're registered!</h3>";
     }
     // Retrieve data
-    $sql_select = "SELECT * FROM registration_tbl WHERE customer = "$company"";
+    $sql_select = "SELECT * FROM registration_tbl";
     $stmt = $conn->query($sql_select);
     $registrants = $stmt->fetchAll(); 
     if(count($registrants) > 0) {

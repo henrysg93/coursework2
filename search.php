8b72aa11455630b637b9ed1c64dfbe8295cd04f7
HenrySG <henrysg93@gmail.com>
2014-10-26 11:22:17 +0000

    search removed insert data sql

--- search.php
+++ search.php
@@ -43,18 +43,9 @@
     if(!empty($_POST)) {
     try {
         $name = $_POST['name'];
-        $email = $_POST['email'];
-		$company = $_POST['company'];
-        $date = date("Y-m-d");
+       
         // Insert data
-        $sql_insert = "INSERT INTO registration_tbl (name, email, date, company) 
-                   VALUES (?,?,?,?)";
-        $stmt = $conn->prepare($sql_insert);
-        $stmt->bindValue(1, $name);
-        $stmt->bindValue(2, $email);
-        $stmt->bindValue(3, $date);
-		$stmt->bindValue(4, $company);
-        $stmt->execute();
+        
     }
     catch(Exception $e) {
         die(var_dump($e));
